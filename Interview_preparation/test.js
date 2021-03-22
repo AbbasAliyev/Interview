74bff5ce3ccbabe87e7314e6d9d4b925f798a85b
Abbas Aliyev <abbas@Abbass-MacBook-Pro.local>
2021-03-22 08:53:08 -0700

    Updates

--- Interview_preparation/test.js
+++ Interview_preparation/test.js
@@ -3,12 +3,20 @@
 //
 // console.log(i)
 
-const arr = [110, 4, 22, 421, 77, 90, 12, 42];
-const arr2 = [];
-for(let i = 0; i <= arr.length; i++){
-    if(arr[i] % 2 === 0){
-        arr2.push(arr[i])
-    }
+// const arr = [110, 4, 22, 421, 77, 90, 12, 42];
+// const arr2 = [];
+// for(let i = 0; i <= arr.length; i++){
+//     if(arr[i] % 2 === 0){
+//         arr2.push(arr[i])
+//     }
+//
+// }
+// console.log(arr2)
 
+let name = [A, b, b, a, s];
+for(let i = 0; i < name.length; i++){
+    if(name[i] === b){
+        name.push("d")
+    }
 }
-console.log(arr2)
+console.log(name);
